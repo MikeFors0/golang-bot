ee1f8a0138e70a91c10e7aee41e0fc29ce3e5841
irsCooper <spock187sheldon@gmail.com>
2023-10-16 13:17:50 +0400

    bot

--- pkg/telegram/command.go
+++ pkg/telegram/command.go
@@ -1,8 +1,8 @@
 package telegram
 
 import (
+	"fmt"
 	"log"
-	"strings"
 
 	"github.com/MikeFors0/golang-bot/database"
 	"github.com/MikeFors0/golang-bot/models"
@@ -23,49 +23,13 @@ func (b *Bot) Start() error {
 	return nil
 }
 
+
 //переделать под работу с БД!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 //авторизация, пользователь вводит свой логин и пароль 
 //может сделать это через пробел, или с переносом на следующую строку
 func (b *Bot) Reg(message *tgbotapi.Message) error {
-	var (
-		login string
-		password string
-	)
-
-	if text := strings.Split(message.Text, "\n"); len(text) == 2 {
-		if err := strings.Split(text[0], "@"); len(err) == 1 {
-			Reset_User_Command(message.From.ID, "reset_login")
-			log.Printf("new command: reset_login")
-			b.setMessage(message, "Логин указан неверно, пожалуйста, повторите попытку.\nНапишите логин и пароль ещё раз.")
-			return nil
-		}
-
-		login = text[0]
-		password = text[1]
-
-	} else if len(text) == 1 {
-		_text := strings.Split(message.Text, " ") 
-		if len(_text) != 2 {
-			Reset_User_Command(message.From.ID, "reset_login")
-			log.Printf("new command: reset_login")
-			b.setMessage(message, "Данные указаны неверно, повторите попытку ещё раз.")
-			return nil
-		}
-
-		if err := strings.Split(text[0], "@"); len(err) == 1 {
-			Reset_User_Command(message.From.ID, "reset_login")
-			log.Printf("new command: reset_login")
-			b.setMessage(message, "Логин указан неверно, пожалуйста, повторите попытку.\nНапишите логин и пароль ещё раз.")
-			return nil
-		}
-
-		login = _text[0]
-		password = _text[1]
-	
-	} else {
-		Reset_User_Command(message.From.ID, "reset_login")
-		log.Printf("new command: reset_login")
-		b.setMessage(message, "Данные указаны неверно, повторите попытку ещё раз.")
+	login, password := b.handleLogin(message)
+	if login == "" || password == "" {
 		return nil
 	}
 
@@ -74,10 +38,10 @@ func (b *Bot) Reg(message *tgbotapi.Message) error {
 
 
 	_, err := database.AuthenticateUser(login, password)
-	if err == nil {
-		database.AddUser(&models.User{Login: login, Password: password})
+	if err != nil {
+		database.AddUser(&models.User{Login: login, Password: password, User_ID: fmt.Sprint(message.From.ID)})
 	} else {
-		if err.Error() == "" {
+		if err.Error() == "invalid password" {
 			Reset_User_Command(message.From.ID, "reset_login")
 			b.setMessage(message, "Неправильный проль, повторите попытку ещё раз.")
 		}
--- pkg/telegram/handlers.go
+++ pkg/telegram/handlers.go
@@ -1,10 +1,11 @@
 package telegram
 
 import (
+	"log"
+	"strings"
+
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
-
-
 
 //обработчик сообщений
 func (b *Bot) handleMessage(message *tgbotapi.Message) error {
@@ -17,6 +18,7 @@ func (b *Bot) handleMessage(message *tgbotapi.Message) error {
 	switch command_user.Command {
 	case "start":
 		return b.Reg(message)
+
 	case "reset_login":
 		return b.Reg(message)
 
@@ -87,6 +89,52 @@ func (b *Bot) handleStart(message *tgbotapi.Message) error {
 
 
 
+func (b *Bot) handleLogin(message *tgbotapi.Message) (string, string) {
+	var (
+		login string
+		password string
+	)
+
+
+	if text := strings.Split(message.Text, "\n"); len(text) == 2 {
+		if err := strings.Split(text[0], "@"); len(err) == 1 {
+			Reset_User_Command(message.From.ID, "reset_login")
+			log.Printf("new command: reset_login")
+			b.setMessage(message, "Логин указан неверно, повторите попытку ещё раз.")
+			return "", ""
+		}
+
+		login = text[0]
+		password = text[1]
+
+	} else if len(text) == 1 {
+		_text := strings.Split(message.Text, " ") 
+		if len(_text) != 2 {
+			Reset_User_Command(message.From.ID, "reset_login")
+			log.Printf("new command: reset_login")
+			b.setMessage(message, "Данные указаны неверно, повторите попытку ещё раз.")
+			return "", ""
+		}
+
+		if err := strings.Split(text[0], "@"); len(err) == 1 {
+			Reset_User_Command(message.From.ID, "reset_login")
+			log.Printf("new command: reset_login")
+			b.setMessage(message, "Логин указан неверно, повторите попытку ещё раз.")
+			return "", ""
+		}
+
+		login = _text[0]
+		password = _text[1]
+	
+	} else { 
+		Reset_User_Command(message.From.ID, "reset_login")
+		log.Printf("new command: reset_login")
+		b.setMessage(message, "Данные указаны неверно, повторите попытку ещё раз.")
+		return "", ""
+	}
+
+	return login, password
+}
 
 
 
